c6dd9a9964cd2743730a49b7b176069b3433b5fa
Robert Kasper <roberthkasper@gmail.com>
2025-01-10 13:23:06 -0500

    UI implementation for menu map selection finished

--- Assets/MainMenu/CustomMapListDisplayElement.cs
+++ Assets/MainMenu/CustomMapListDisplayElement.cs
@@ -1,13 +1,21 @@
+using System;
 using MapMaker;
 using RKUnityToolkit.UIElements;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MainMenu
 {
     public class CustomMapListDisplayElement : GenericListDisplay.ListItemController<CustomMap>
     {
         [SerializeField] private TextMeshProUGUI mapNameText;
+        [SerializeField] private Toggle toggle;
+        
+        private void Start()
+        {
+            toggle.group = GetComponentInParent<ToggleGroup>();
+        }
         
         protected override void OnDataSet(CustomMap data)
         {
--- Assets/MainMenu/CustomMapOptionsUiController.cs
+++ Assets/MainMenu/CustomMapOptionsUiController.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using MapMaker;
 using RKUnityToolkit.UIElements;
+using RKUnityToolkit.UnityExtensions;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MainMenu
 {
@@ -16,9 +19,26 @@ namespace MainMenu
             mapsListDisplay.DisplayList(LoadCustomMaps(), customMapDisplayElementPrefab);
         }
 
+        public void OnClickStartGameButton()
+        {
+            var toggles = mapsListDisplay.GetComponentsInDirectChildren<Toggle>();
+            var activeToggle = toggles.FirstOrDefault(t => t.isOn);
+
+            if (activeToggle)
+            {
+                var selectedMap = activeToggle.GetComponent<CustomMapListDisplayElement>().Data;
+                StartCustomMapGame(selectedMap);
+            }
+        }
+
         private List<CustomMap> LoadCustomMaps()
         {
             return Resources.LoadAll<CustomMap>(CustomMap.CustomMapsResourcesFolder).ToList();
+        }
+
+        private void StartCustomMapGame(CustomMap customMap)
+        {
+            throw new NotImplementedException($"Todo: Start game from Custom Map: \"{customMap.gameObject.name}\"");
         }
     }
 }
