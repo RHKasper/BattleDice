f1905aef8206145030373b9360b065ad2fcdfb84
Robert Kasper <roberthkasper@gmail.com>
2025-01-20 18:30:50 -0500

    bugfix for ai + values growing reinforcement region

--- Assets/BattleDataModel/AiPlayerStrategies/AggressiveAiStrategy.cs
+++ Assets/BattleDataModel/AiPlayerStrategies/AggressiveAiStrategy.cs
@@ -9,14 +9,15 @@ namespace BattleDataModel.AiPlayerStrategies
         public override void PlayNextMove(Battle battle, Player player)
         {
             List<MapNode> myTerritories = battle.Map.GetTerritoriesOwnedByPlayer(player.PlayerIndex);
+            HashSet<MapNode> myReinforcementTerritories = battle.Map.GetLargestContiguousGroupOfTerritories(player.PlayerIndex);
             PotentialAttack bestAttackOption = null;
 
             foreach (MapNode territory in myTerritories)
             {
-                IEnumerable<MapNode> attackCandidates = territory.AdjacentNodes.Where(t => t.OwnerPlayerIndex != player.PlayerIndex && t.NumDice < territory.NumDice);
+                IEnumerable<MapNode> attackCandidates = territory.AdjacentNodes.Where(t => t.OwnerPlayerIndex != player.PlayerIndex);
                 foreach (MapNode attackCandidate in attackCandidates)
                 {
-                    var score = GetDesirabilityScoreForPotentialAttack(territory, attackCandidate, battle, myTerritories);
+                    var score = GetDesirabilityScoreForPotentialAttack(territory, attackCandidate, battle, myReinforcementTerritories);
 
                     if (score > 0)
                     {
@@ -42,7 +43,7 @@ namespace BattleDataModel.AiPlayerStrategies
         /// Calculates a score for how desirable a particular attack would be. Values less than or equal to 0 are a "no".
         /// The highest scoring attack above 0 will be chosen 
         /// </summary>
-        public float GetDesirabilityScoreForPotentialAttack(MapNode attackingTerritory, MapNode defendingTerritory, Battle battle, List<MapNode> myTerritories)
+        public float GetDesirabilityScoreForPotentialAttack(MapNode attackingTerritory, MapNode defendingTerritory, Battle battle, HashSet<MapNode> myReinforcementTerritories)
         {
             float score = 1;
 
@@ -55,6 +56,12 @@ namespace BattleDataModel.AiPlayerStrategies
             
             // if target territory is part of an enemy's largest region, rate it higher
             if (battle.Map.GetLargestContiguousGroupOfTerritories(defendingTerritory.OwnerPlayerIndex).Contains(defendingTerritory))
+            {
+                score++;
+            }
+            
+            // if owning target territory would add 2+ territories to the attacker's largest region, rate it higher
+            if (myReinforcementTerritories.Contains(attackingTerritory) && defendingTerritory.AdjacentNodes.Any(t => t.OwnerPlayerIndex == attackingTerritory.OwnerPlayerIndex && !myReinforcementTerritories.Contains(t)))
             {
                 score++;
             }
