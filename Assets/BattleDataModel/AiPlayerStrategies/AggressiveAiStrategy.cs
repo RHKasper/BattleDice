8fef3b507d41c2b296fe2c2b2147419407e04cee
Robert Kasper <roberthkasper@gmail.com>
2025-01-20 19:40:38 -0500

    reorganize inheritance for ai strategies

--- Assets/BattleDataModel/AiPlayerStrategies/AggressiveAiStrategy.cs
+++ Assets/BattleDataModel/AiPlayerStrategies/AggressiveAiStrategy.cs
@@ -6,44 +6,11 @@ namespace BattleDataModel.AiPlayerStrategies
 {
     public class AggressiveAiStrategy : AiPlayerStrategyBase
     {
-        public override void PlayNextMove(Battle battle, Player player)
-        {
-            List<MapNode> myTerritories = battle.Map.GetTerritoriesOwnedByPlayer(player.PlayerIndex);
-            HashSet<MapNode> myReinforcementTerritories = battle.Map.GetLargestContiguousGroupOfTerritories(player.PlayerIndex);
-            PotentialAttack bestAttackOption = null;
-
-            foreach (MapNode territory in myTerritories)
-            {
-                IEnumerable<MapNode> attackCandidates = territory.AdjacentNodes.Where(t => t.OwnerPlayerIndex != player.PlayerIndex);
-                foreach (MapNode attackCandidate in attackCandidates)
-                {
-                    var score = GetDesirabilityScoreForPotentialAttack(territory, attackCandidate, battle, myReinforcementTerritories);
-
-                    if (score > 0)
-                    {
-                        if (bestAttackOption == null || score > bestAttackOption.DesirabilityScore)
-                        {
-                            bestAttackOption = new PotentialAttack(territory, attackCandidate, score);
-                        }
-                    }
-                }
-            }
-
-            if (bestAttackOption != null)
-            {
-                battle.Attack(bestAttackOption.Attacker, bestAttackOption.Defender);
-            }
-            else
-            {
-                battle.EndTurn();
-            }
-        }
-
         /// <summary>
         /// Calculates a score for how desirable a particular attack would be. Values less than or equal to 0 are a "no".
         /// The highest scoring attack above 0 will be chosen 
         /// </summary>
-        public float GetDesirabilityScoreForPotentialAttack(MapNode attackingTerritory, MapNode defendingTerritory, Battle battle, HashSet<MapNode> myReinforcementTerritories)
+        protected override float GetDesirabilityScoreForPotentialAttack(MapNode attackingTerritory, MapNode defendingTerritory, Battle battle, HashSet<MapNode> myReinforcementTerritories)
         {
             float score = 1;
 
@@ -72,20 +39,6 @@ namespace BattleDataModel.AiPlayerStrategies
             float chanceOfSuccess = GetNormalizedChanceOfWinningAttack(attackingTerritory, defendingTerritory);
             
             return score * chanceOfSuccess;
-        }
-        
-        private class PotentialAttack
-        {
-            public MapNode Attacker;
-            public MapNode Defender;
-            public float DesirabilityScore;
-
-            public PotentialAttack(MapNode attacker, MapNode defender, float desirabilityScore)
-            {
-                Attacker = attacker;
-                Defender = defender;
-                DesirabilityScore = desirabilityScore;
-            }
         }
     }
 }
--- Assets/BattleDataModel/AiPlayerStrategyBase.cs
+++ Assets/BattleDataModel/AiPlayerStrategyBase.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
 
@@ -6,7 +8,40 @@ namespace BattleDataModel
 {
     public abstract class AiPlayerStrategyBase
     {
-        public abstract void PlayNextMove(Battle battle, Player player);
+        public void PlayNextMove(Battle battle, Player player)
+        {
+            List<MapNode> myTerritories = battle.Map.GetTerritoriesOwnedByPlayer(player.PlayerIndex);
+            HashSet<MapNode> myReinforcementTerritories = battle.Map.GetLargestContiguousGroupOfTerritories(player.PlayerIndex);
+            PotentialAttack bestAttackOption = null;
+
+            foreach (MapNode territory in myTerritories)
+            {
+                IEnumerable<MapNode> attackCandidates = territory.AdjacentNodes.Where(t => t.OwnerPlayerIndex != player.PlayerIndex);
+                foreach (MapNode attackCandidate in attackCandidates)
+                {
+                    var score = GetDesirabilityScoreForPotentialAttack(territory, attackCandidate, battle, myReinforcementTerritories);
+
+                    if (score > 0)
+                    {
+                        if (bestAttackOption == null || score > bestAttackOption.DesirabilityScore)
+                        {
+                            bestAttackOption = new PotentialAttack(territory, attackCandidate, score);
+                        }
+                    }
+                }
+            }
+
+            if (bestAttackOption != null)
+            {
+                battle.Attack(bestAttackOption.Attacker, bestAttackOption.Defender);
+            }
+            else
+            {
+                battle.EndTurn();
+            }
+        }
+        
+        protected abstract float GetDesirabilityScoreForPotentialAttack(MapNode attackingTerritory, MapNode defendingTerritory, Battle battle, HashSet<MapNode> myReinforcementTerritories);
 
         /// <summary>
         /// Returns the odds of winning an attack. When the attacker has fewer dice, 0 is returned.
@@ -46,6 +81,20 @@ namespace BattleDataModel
 
                 _ => throw new ArgumentOutOfRangeException()
             };
+        }
+        
+        private class PotentialAttack
+        {
+            public MapNode Attacker;
+            public MapNode Defender;
+            public float DesirabilityScore;
+
+            public PotentialAttack(MapNode attacker, MapNode defender, float desirabilityScore)
+            {
+                Attacker = attacker;
+                Defender = defender;
+                DesirabilityScore = desirabilityScore;
+            }
         }
     }
 }
