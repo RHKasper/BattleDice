981e8fff4a07938c56d054694cf9ef838252aa6a
Robert Kasper <RobertHKasper@gmail.com>
2025-04-29 14:06:58 -0400

    WIP programming Players Setup UI behavior

---
+++ Assets/MainMenu/MapsScreen/PlayersSetupUIController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MainMenu.MapsScreen
+{
+    public class PlayersSetupUIController : MonoBehaviour
+    {
+        [SerializeField] private Button addPlayerButton;
+        [SerializeField] private Button removePlayerButton;
+        [SerializeField] private Image numPlayersImage;
+        
+        
+        public void OnClickAddPlayer()
+        {
+            // validate player count change
+            // change player count
+            // activate/deactive buttons
+            
+        }
+
+        public void OnClickRemovePlayer()
+        {
+            
+        }
+    }
+}
