79941460d80ca6ae9aa3750c99d6837978115ae3
Robert Kasper <RobertHKasper@gmail.com>
2025-05-13 16:57:29 -0400

    dice color update

--- Assets/GlobalScripts/Constants.cs
+++ Assets/GlobalScripts/Constants.cs
@@ -7,26 +7,27 @@ namespace GlobalScripts
     // todo: give this a better name since it's got stuff loaded from resources
     public static class Constants
     {
-        public const int MinPlayers = 1;
-        public const int MaxPlayers = 12;
-
         public static readonly List<Color> Colors = new()
         {
-            new Color(31/255f, 120/255f, 180/255f),
+            new Color(227/255f, 26/255f, 28/255f),
             new Color(178/255f, 223/255f, 138/255f),
             new Color(51/255f, 160/255f, 44/255f),
             new Color(251/255f, 154/255f, 153/255f),
-            new Color(227/255f, 26/255f, 28/255f),
             new Color(253/255f, 191/255f, 111/255f),
             new Color(255/255f, 127/255f, 0/255f),
             new Color(166/255f, 206/255f, 227/255f),
             new Color(202/255f, 178/255f, 214/255f),
             new Color(106/255f, 61/255f, 154/255f),
             new Color(255/255f, 255/255f, 153/255f),
-            new Color(177/255f, 89/255f, 40/255f)
+            new Color(177/255f, 89/255f, 40/255f),
+            new Color(31/255f, 120/255f, 180/255f)
         };
-        
-        
+
+        public const int MinPlayers = 1;
+
+        public const int MaxPlayers = 12;
+
+
         public static string ResourcesDirectory => Path.Combine(Application.dataPath, "Resources");
         
         public static string GetFaceSpriteFilePath(int playerIndex, int dieValue)
